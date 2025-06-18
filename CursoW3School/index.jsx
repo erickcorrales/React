2261de87672ef667631529ec270388c1652b1372
erickcorrales <erickcorriales88@gmail.com>
2025-06-17 22:12:51 -0700

    CursoW3School: Empezando con el curso de React

---
+++ CursoW3School/index.jsx
@@ -0,0 +1,13 @@
+
+function Hello() {
+    return <h1>Hola mundo!</h1>
+}
+
+function Adios() {
+    return <h2>Adios</h2>
+    
+}
+
+const container = document.getElementById('mi-div')
+const root = ReactDOM.createRoot(container)
+root.render(<Hello />)
