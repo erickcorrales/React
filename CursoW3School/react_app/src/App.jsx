b0edced4604183a8402953df0a0d6dd27c83eb4a
erickcorrales <erickcorriales88@gmail.com>
2025-06-19 11:50:40 -0700

    Curso PildorasInformaticas: Ejemplo de programa sin React, creando el programa de ejemplo con React

---
+++ CursoW3School/react_app/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+
+export default function App() {
+    return (
+        <div className="App">
+            <h1>Hello World from a Function!</h1>
+        </div>
+    )
+}
+
+export function Get() {
+    return (
+        <p>Parrafo desde una funcion de React</p>
+    )
+}
+
+/* export default App */
